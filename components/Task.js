bd92bb253f634adcef1768352f845d9a6bd00bda
Arianne Roselina Prananto <arianne.prananto@cosateq.com>
2023-05-29 20:13:50 +0200

    finish UI

---
+++ components/Task.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Task = (props) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row', // align items on the left with the circular side by side
+    alignItems: 'center',
+    justifyContent: 'space-between', // push text and circular to opposite ends
+    marginBottom: 20, // space between tasks
+  },
+  itemLeft: { // align square and text side by side
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap', // wrap content to the next line if it's too long
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: '#55BCF6',
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: '80%', // limit the text's space
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: '#55BCF6',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
+
+export default Task;
